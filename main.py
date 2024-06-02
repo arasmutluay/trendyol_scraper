c87ecd6fbf2c521980f6b3553437576496cf8838
arasmutluay <arasmutluayy@gmail.com>
2024-06-03 00:16:35 +0300

    Fixed functions, added comments and changed category

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
 from selenium.common import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
-
 from bs4 import BeautifulSoup
 import requests
 from selenium.webdriver.support.wait import WebDriverWait
@@ -13,7 +11,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 def scrape_product_details(product_url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-
     driver = webdriver.Chrome(options=chrome_options)
 
     driver.get("https://www.trendyol.com" + product_url)
@@ -22,10 +19,11 @@ def scrape_product_details(product_url):
     print("Name: " + name)
     price = driver.find_element(By.CSS_SELECTOR, "span.prc-dsc").text.strip()
     print("Price: " + price)
-    category = driver.find_element(By.CSS_SELECTOR, "div.product-detail-breadcrumb.full-width").text.strip()
+    category = driver.find_element(By.CSS_SELECTOR, "div.product-detail-breadcrumb.full-width").text.strip().split('\n')[-1]
     print("Category: " + category)
 
-    rating = None
+    # we wait 5 sec because rating and comments load after the static HTML is loaded.
+    rating = None  # because some products do not have comment or rating
     try:
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "p.p-reviews-rate-text")))
         rating = driver.find_element(By.CSS_SELECTOR, "p.p-reviews-rate-text").text.strip()
@@ -62,6 +60,8 @@ def scrape_products():
     page_number = 1
 
     while len(all_products) < 200:
+        print(f"Scraping Page Number: {page_number}")
+
         url = base_url.format(page_number)
         result = requests.get(url)
         doc = BeautifulSoup(result.content, "html.parser")
@@ -69,17 +69,9 @@ def scrape_products():
         product_containers = doc.find_all("div", class_="p-card-chldrn-cntnr card-border")
 
         for product in product_containers:
-            product_url = product.find("a")["href"]  # To get the product url and pass it to scrape_product_details func
-            name_tag = product.find("span", class_="prdct-desc-cntnr-ttl")
-            description_tag = product.find("span", class_="prdct-desc-cntnr-name")
-            price_tag = product.find("div", class_="prc-box-dscntd")
-
-            if name_tag and description_tag and price_tag:
-                name = name_tag.get_text(strip=True) + " " + description_tag.get_text(strip=True)
-                price = price_tag.get_text(strip=True)
-                product_details = scrape_product_details(product_url)
-                product_details["price"] = price
-                all_products.append(product_details)
+            product_url = product.find("a")["href"]  # Get the URL href for each product,
+            product_details = scrape_product_details(product_url)  # and pass it to scrape_product_details
+            all_products.append(product_details)
 
             if len(all_products) >= 200:
                 break
@@ -88,5 +80,5 @@ def scrape_products():
 
     return all_products
 
-scrape_products()
 
+scrape_products()
